d09216703d6c7d241331e6b441b046e8bf02cc5f
Gogulaanand R <gogulaanand02@gmail.com>
2020-03-17 10:44:53 +0530

    recenter the point of hand rotation

--- clock.js
+++ clock.js
@@ -2,20 +2,19 @@ const current = new Date();
 let init_hrs = current.getHours();
 let init_mins = current.getMinutes();
 let init_seconds = current.getSeconds();
-var clock = document.getElementById('clock');
-var hr_hand = document.getElementById('hour');
-var min_hand = document.getElementById('minute');
-var sec_hand = document.getElementById('second');
-
+var clock = document.getElementById('clock'),
+    hr_hand = document.getElementById('hour'),
+    min_hand = document.getElementById('minute'),
+    sec_hand = document.getElementById('second'),
+    hr_deg = (init_hrs%12 || 12)*30,
+    min_deg = init_mins*6,
+    sec_deg = init_seconds*6;
 
 function setup(){
-	var hr_deg = (init_hrs%12 || 12)*30;
-	var min_deg = init_mins*6;
-    var sec_deg = init_seconds*6;
     hr_hand.style.transform = `rotate(${hr_deg}deg)`;
 	min_hand.style.transform = `rotate(${min_deg}deg)`;
 	sec_hand.style.transform = `rotate(${sec_deg}deg)`;
-    hr_hand.offsetHeight;
 }
 
 window.addEventListener('load', setup);
+
