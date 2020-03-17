413b4969f0bbf1df9b4ce4666754f40252612db0
Gogulaanand R <gogulaanand02@gmail.com>
2020-03-17 23:11:06 +0530

    completed logic plus UI modifications

--- clock.js
+++ clock.js
@@ -1,20 +1,19 @@
-const current = new Date();
-let init_hrs = current.getHours();
-let init_mins = current.getMinutes();
-let init_seconds = current.getSeconds();
+
 var clock = document.getElementById('clock'),
     hr_hand = document.getElementById('hour'),
     min_hand = document.getElementById('minute'),
-    sec_hand = document.getElementById('second'),
-    hr_deg = (init_hrs%12 || 12)*30,
-    min_deg = init_mins*6,
-    sec_deg = init_seconds*6;
+    sec_hand = document.getElementById('second');
 
 function setup(){
+    const current = new Date();
+    var hr_deg = (current.getHours()%12 || 12)*30,
+        min_deg = current.getMinutes()*6,
+        sec_deg = current.getSeconds()*6;
     hr_hand.style.transform = `rotate(${hr_deg}deg)`;
 	min_hand.style.transform = `rotate(${min_deg}deg)`;
-	sec_hand.style.transform = `rotate(${sec_deg}deg)`;
+    sec_hand.style.transform = `rotate(${sec_deg}deg)`;
 }
 
 window.addEventListener('load', setup);
 
+setInterval(setup, 1000);
